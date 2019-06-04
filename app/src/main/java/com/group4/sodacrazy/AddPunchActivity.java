f996d3dca9ecc8e0b58831288ea52ed578e2e3e5
Tori Fife <torifife@gmail.com>
2019-06-04 17:24:27 -0600

    Added comments to what I committed earlier. Also, in the last commit I added an icon, too. So there's that.

--- app/src/main/java/com/group4/sodacrazy/AddPunchActivity.java
+++ app/src/main/java/com/group4/sodacrazy/AddPunchActivity.java
@@ -23,35 +23,41 @@ import java.io.IOException;
 
 public class AddPunchActivity extends AppCompatActivity {
 
-    SurfaceView surfaceView;
-    CameraSource cameraSource;
-    TextView txtView;
-    BarcodeDetector detector;
+    SurfaceView surfaceView; //where the camera will show up
+    CameraSource cameraSource; //this lets us use the camera on a very basic level (which is all we need)
+    TextView txtView; //display text (eventually might have instructions or something)
+    BarcodeDetector detector; //so we can read barcodes. This is part of the google play services thing
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_punch);
 
-        Intent intent = getIntent();
+        Intent intent = getIntent(); //I don't know if this does anything for us
 
         surfaceView = (SurfaceView) findViewById(R.id.cameraPreview);
         txtView = (TextView) findViewById(R.id.txtview);
+
+        //make a detector that will look for qr codes
         detector = new BarcodeDetector.Builder(getApplicationContext())
                 .setBarcodeFormats(Barcode.QR_CODE).build();
 
+        //give details about what we want from our camera source
         cameraSource = new CameraSource.Builder(this, detector)
                 .setRequestedPreviewSize(640, 480)
                 .setFacing(CameraSource.CAMERA_FACING_BACK)
                 .build();
 
+        //create our SurfaceView
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
+                //if we don't have permission, we can't do anything. This will show a black screen
                 if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     return;
                 }
                 try {
+                    //access the camera
                     cameraSource.start(holder);
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -60,11 +66,12 @@ public class AddPunchActivity extends AppCompatActivity {
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+                //extra functions just have to be overridden, so they can't be deleted. They don't do anything.
             }
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
+                //stop the camera
                 cameraSource.stop();
 
             }
@@ -73,23 +80,33 @@ public class AddPunchActivity extends AppCompatActivity {
         detector.setProcessor(new Detector.Processor<Barcode>() {
             @Override
             public void release() {
-
+                //extra functions just have to be overridden, so they can't be deleted. They don't do anything.
             }
 
+            //react to the qr codes we see
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
                 final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+                //if we have any qr code(s) on the screen...
                 if (qrCodes.size() != 0) {
                     txtView.post(new Runnable() {
                         @Override
-                        public void run() {
-                            txtView.setText(qrCodes.valueAt(0).rawValue);
+                        public void run() { //Ok. I DO NOT want to do anything with a textview. I'll fix this later.
+                            txtView.setText(qrCodes.valueAt(0).rawValue); // yeah this will def change
+                            //if the 1st (usually only) qr code on the screen matches the secret code...
                             if (qrCodes.valueAt(0).rawValue.equals("soda34234298432075892780324932849038249032890483290crazy")) {
+                                //vibrate a bit to let the user know it worked
                                 Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                                vibrator.vibrate(100);cameraSource.stop();
+                                vibrator.vibrate(100);
+                                //stop the camera source (this might not be necessary)
+                                cameraSource.stop();
+
+                                //give information back to the activity that called this one
                                 Intent output = new Intent();
-                                output.putExtra("Punch", 1);
-                                setResult(RESULT_OK, output);
+                                output.putExtra("Punch", 1); //right now this will always be 1
+                                setResult(RESULT_OK, output); //say things went well, pass back the intent
+
+                                //exit this activity (and return to the one that called it)
                                 finish();
                             }
                         }
--- app/src/main/java/com/group4/sodacrazy/PunchCardActivity.java
+++ app/src/main/java/com/group4/sodacrazy/PunchCardActivity.java
@@ -21,11 +21,19 @@ public class PunchCardActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_punch_card);
-        Intent intent = getIntent();
+        Intent intent = getIntent(); //this might do nothing. Not sure.
+
+        //for TESTING, this is automatically put to 0. In real life, it will need to be a value
+        //that is stored even when the app is closed.
         punches = 0;
 
     }
 
+    /**
+     * punchCard is called when the button to punch is pressed. It calls AddPunchActivity (the
+     * barcode screen) with the intention of getting a result, which will be the number of
+     * punches to add (right now always 1).
+     */
     public void punchCard(View view) {
         boolean ready = false;
         if (ContextCompat.checkSelfPermission(this,
@@ -35,22 +43,35 @@ public class PunchCardActivity extends Activity {
                     new String[]{Manifest.permission.CAMERA}, 1);
         }
 
+        // this loop ensures that the new activity won't start until the user has given permission to
+        // use the camera. Unfortunately, it currently causes a freeze if the user clicks "Deny," so
+        // we probably need a better solution for this
         while(ContextCompat.checkSelfPermission(this,
                 Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
         }
+
         ready = true;
         if (ready) {
             Intent intent = new Intent(this, AddPunchActivity.class);
+            // this enables us to get whatever information BACK that we want (it receives an intent
+            // back)
             startActivityForResult(intent, ADD_PUNCH_REQUEST);
         }
     }
 
+    /**
+     * This is what the activity will do when it gets a result from a called activity
+     */
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //if the result is from the Add Punch activity...
         if (requestCode == ADD_PUNCH_REQUEST) {
+            //RESULT_OK is passed back from the other activity
             if(resultCode == RESULT_OK) {
                 txtView = findViewById(R.id.textView);
                 punches += data.getIntExtra("Punch", 0);
+
+                //this is temporary--just to show a countdown
                 if (punches >= 10) {
                     punches -= 10;
                 }
