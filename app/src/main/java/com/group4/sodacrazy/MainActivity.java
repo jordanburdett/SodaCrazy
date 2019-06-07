717ed6cb17fefcb99f5c1934e3e68f495cabe17e
jordanburdett <jordan@burdett.us>
2019-06-07 09:41:33 -0600

    Added a recyclerView and created a new branch for testing it. Some tweaks to the theme.
    updated androidx.test:runner to latest version
    updated androidx.test.espresso to latest version

--- app/src/main/java/com/group4/sodacrazy/MainActivity.java
+++ app/src/main/java/com/group4/sodacrazy/MainActivity.java
@@ -3,8 +3,11 @@ package com.group4.sodacrazy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     /**
