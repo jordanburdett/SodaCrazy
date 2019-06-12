7ed12a2e095d7f085639d03c2c19002d8106e820
Tori Fife <torifife@gmail.com>
2019-06-12 12:04:48 -0600

    Added navigation functionality to Menu activity. Appearance should be changed later to be consistent with the rest of the app

--- app/src/main/java/com/group4/sodacrazy/MenuActivity.java
+++ app/src/main/java/com/group4/sodacrazy/MenuActivity.java
@@ -1,6 +1,8 @@
 package com.group4.sodacrazy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,25 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    /**
+     * This is for testing the punch card stuff. It can be changed to whatever menu thing we have.
+     * Just make sure that whatever button is supposed to go to the punch card screen does this
+     * method.
+     * **/
+    public void toPunch(View view) {
+        Intent intent = new Intent(this, PunchCardActivity.class);
+        startActivity(intent);
+    }
+
+    public void toBuild(View view) {
+        Intent intent = new Intent(this, BuildActivity.class);
+        startActivity(intent);
+    }
+
+    public void toMenu(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
