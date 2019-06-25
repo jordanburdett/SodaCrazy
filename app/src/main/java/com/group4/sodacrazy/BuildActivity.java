48fc409edc16e356f24e90b36852c4be5f34dd8a
FiscalMonkey <wabbie@byui.edu>
2019-06-25 13:39:40 -0600

    Explore different options on spinners for build menu

--- app/src/main/java/com/group4/sodacrazy/BuildActivity.java
+++ app/src/main/java/com/group4/sodacrazy/BuildActivity.java
@@ -1,10 +1,16 @@
 package com.group4.sodacrazy;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BuildActivity extends AppCompatActivity {
+
+    // declare array adapters that will change the value of the
+    // spinners in the activity
+    private ArrayAdapter<String>beverage_spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/group4/sodacrazy/SpinnerActivity.java
@@ -0,0 +1,15 @@
+package com.group4.sodacrazy;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+
+public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        switch (pos){
+            case 1:
+
+        }
+    }
+}
