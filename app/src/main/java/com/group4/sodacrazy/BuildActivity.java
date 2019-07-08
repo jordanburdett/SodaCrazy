da4dd356e55777412134cee06dbf81bb47b45e57
FiscalMonkey <mor16112@byui.edu>
2019-07-08 10:02:28 -0600

    Add javadoc for spinners. String array for beverage type spinner set in correct order.

--- app/src/main/java/com/group4/sodacrazy/BuildActivity.java
+++ app/src/main/java/com/group4/sodacrazy/BuildActivity.java
@@ -18,18 +18,37 @@ public class BuildActivity extends AppCompatActivity {
     // declare array adapters that will change the value of the
     // spinners in the activity
     private ArrayAdapter<CharSequence>bev_size_adapter;
-    private int bev_price;
+
+    /**
+     * Initial price for beverage depending on size
+     */
+    private int base_price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_build);
 
-        bev_price = 0;
+        base_price = 0;
 
+        /**
+         * The Spinner responsible for showing the types of beverages that can be selected
+         */
         Spinner beverage_type = (Spinner) findViewById(R.id.beverage_spinner);
+
+        /**
+         * The spinner that will show the appropriate sizes of beverage depending on the selected
+         * beverage type in {@link beverage_type}.
+         */
         final Spinner bev_size = (Spinner) findViewById(R.id.bev_size_spinner);
 
+        /**
+         * Sets the adapter with appropriate array of strings depending on the spinner selection
+         * and applies it to the next spinner so that it may show the appropriate sizes for each
+         * beverage
+         *
+         * @param   AdapterView.OnItemSelectedListener() handles the actual adaptation
+         */
         beverage_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int pos, long id) {
@@ -44,8 +63,8 @@ public class BuildActivity extends AppCompatActivity {
                         bev_size.setAdapter(bev_size_adapter);
                         break;
                     case  3: // Shake It Up
-                    case  4: // Crazy Ice Drinks
-                    case  5: // Floats
+                    case  4: // Crazy Ice Drink
+                    case  5: // Float
                         bev_size_adapter = ArrayAdapter.createFromResource(parentView.getContext(), R.array.shake_crazy_float_sizes, android.R.layout.simple_spinner_dropdown_item);
                         bev_size.setAdapter(bev_size_adapter);
                         break;
@@ -63,7 +82,7 @@ public class BuildActivity extends AppCompatActivity {
                         break;
                     case  9: // Coffee
                     case 10: // Hot Chocolate
-                    case 11: // Steamers
+                    case 11: // Steamer
                     case 12: // Chi Tea
                         bev_size_adapter = ArrayAdapter.createFromResource(parentView.getContext(), R.array.hot_drink_sizes, android.R.layout.simple_spinner_dropdown_item);
                         bev_size.setAdapter(bev_size_adapter);
@@ -77,9 +96,27 @@ public class BuildActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
+                base_price = 0;
+            }
+        });
+
+        /**
+         * Adds appropriate price for the selected size and beverage type
+         *
+         * @param   AdapterView.OnItemSelectedListener()
+         */
+        bev_size.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                switch(/*string that is selected in spinner*/) {
+                    case "":
+                }
             }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
         });
     }
 
